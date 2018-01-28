c0f4158260a008ad7b6bcf1dc868c7301c292e36
julio177 <julioc.aguilarv@gmail.com>
2018-01-28 13:34:27 -0600

    Agrega animaciones

---
+++ Assets/Scripts/SfxManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxManager : MonoBehaviour {
+
+	// Use this for initialization
+	public AudioClip[] clipList;
+	private AudioSource sfx;
+	void Start () {
+		var dialogComponent = FindObjectOfType<DialogComponent>();
+        dialogComponent.stateChange += OnStateChange;
+	}
+
+	void Awake() {
+		sfx = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnStateChange(int state) {
+
+    }
+}
