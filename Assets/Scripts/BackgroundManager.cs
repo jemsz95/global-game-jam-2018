aa98d5f3c3d28186623cea90411f7ee859895d83
julio177 <julioc.aguilarv@gmail.com>
2018-01-28 12:41:26 -0600

    Background manager funcionando

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BackgroundManager : MonoBehaviour {
 
 	// Use this for initialization
 	public Sprite[] spriteList;
-
+	private Image background;
 	void Start () {
 		var dialogComponent = FindObjectOfType<DialogComponent>();
 		dialogComponent.stateChange += OnStateChange;
+	}
+
+	void Awake() {
+		background = GetComponent<Image>();
 	}
 	
 	// Update is called once per frame
@@ -18,6 +23,7 @@ public class BackgroundManager : MonoBehaviour {
 	}
 
 	private void OnStateChange(int state) {
-		
+		Debug.Log(state);
+		background.sprite = spriteList[state];
 	}
 }
